40910602f7c14f660d93cd6ae19da77577a1bd49
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2021-12-13 12:43:24 +0300

    s

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+host = "127.0.0.1"
+port = 80
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+
+s.connect((host,port))
+
+while 1:
+
+    data = input("Mssage:")
+
+    if data.encode("utf-8")=="exit":
+        print("Bir kullanıcı ayrıldı.")
+    else:
+        s.sendall(data.encode("utf-8"))
+
+    
+    ldata = s.recv(1024)
+
+    if ldata.decode("utf-8") == "exit":
+        print("Bir kullanıcı ayrıldı.")
+
+    else:
+        ldata.decode("utf-8")
